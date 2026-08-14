80852bf13ef4e6906a013180ff5a2b4bd52034e2
Egor Avdeev <Avdeev.eg@icloud.com>
2026-08-14 14:16:35 +0500

    fix: icons, fix: header

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const header = document.querySelector(".header");
+const logo = document.querySelector(".header-logo-img use")
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     header.classList.add("header-light");
+    logo.href.baseVal = "./assets/icons/sprite.svg#logo-dark";
   } else {
     header.classList.remove("header-light");
+    logo.href.baseVal = "./assets/icons/sprite.svg#logo-light";
   }
 });
