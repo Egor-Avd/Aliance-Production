1241b9631deaebfd5d2b51a04b8833c7e2ed2084
Egor Avdeev <Avdeev.eg@icloud.com>
2026-08-14 11:56:51 +0500

    fix: header scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Привет Мир!');
+const header = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  if (this.scrollY > 1) {
+    header.classList.add("header-light");
+  } else {
+    header.classList.remove("header-light");
+  }
+});
